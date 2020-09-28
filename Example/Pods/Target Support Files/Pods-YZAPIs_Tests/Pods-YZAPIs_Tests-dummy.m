0dbea9fe75a2abc7512cafc2090fca1eaf052ee2
jonzhang <jon.zhang@trustlife.com>
2020-09-28 19:49:42 +0800

    apis

---
+++ Example/Pods/Target Support Files/Pods-YZAPIs_Tests/Pods-YZAPIs_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_YZAPIs_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_YZAPIs_Tests
+@end
---
+++ Example/Pods/Target Support Files/YZAPIs/YZAPIs-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_YZAPIs : NSObject
+@end
+@implementation PodsDummy_YZAPIs
+@end
